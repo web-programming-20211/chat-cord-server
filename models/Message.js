6e3f5ed53c6eeef8c2503565094e6e90798a0b93
Phung Dinh Son <62043279+phungson185@users.noreply.github.com>
2021-12-17 11:06:01 +0700

    feat: add urls to msg (#16)

--- models/Message.js
+++ models/Message.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 
 const messageSchema = new mongoose.Schema({
     content: String,
+    urls: [String],
     in: mongoose.Schema.Types.ObjectId,
     from: {
         fullname: String,
