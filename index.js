260f4534f0fb717c5984abc2d60db8c61722b94a
phungson185 <phungson185@gmail.com>
2022-01-11 12:44:12 +0700

    fix

--- index.js
+++ index.js
@@ -50,13 +50,12 @@ app.use(express())
 app.use(express.static(path.join(__dirname + '/client/build')))
 const port = process.env.PORT || 8080
 
-app.use(cors())
-
-app.all('/', function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    next()
-});
+app.use(cors(
+    cors({
+        origin: 'https://chat-cord-api.herokuapp.com',
+        credentials: true,
+    })
+))
 
 app.use(express.urlencoded({
     extended: true
