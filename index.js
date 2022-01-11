1584fd890aa12df0cf82d06d1bbd5ea581e24f4b
phungson185 <phungson185@gmail.com>
2022-01-11 15:41:38 +0700

    fix

--- index.js
+++ index.js
@@ -50,13 +50,8 @@ app.use(express())
 app.use(express.static(path.join(__dirname + '/client/build')))
 const port = process.env.PORT || 8080
 
-app.use(cors({
-    origin: "https://web-programming-20211.github.io/",
-    credentials: true,
-}))
-
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Origin", ['https://web-programming-20211.github.io', 'http://localhost:3000']);
     res.header("Access-Control-Allow-Credentials", true);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
