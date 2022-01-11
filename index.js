fa4ecd5eaafce46641f022118f7b65e09b5ba760
phungson185 <phungson185@gmail.com>
2022-01-11 15:46:01 +0700

    fix

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ app.use(express.static(path.join(__dirname + '/client/build')))
 const port = process.env.PORT || 8080
 
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", ['https://web-programming-20211.github.io', 'http://localhost:3000']);
+    res.header("Access-Control-Allow-Origin", 'https://web-programming-20211.github.io');
     res.header("Access-Control-Allow-Credentials", true);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
