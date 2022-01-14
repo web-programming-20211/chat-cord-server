546b4eab4cb9fd35336751c66cbbb567ae002d92
phungson185 <phungson185@gmail.com>
2022-01-15 01:04:29 +0700

    commit

--- index.js
+++ index.js
@@ -20,20 +20,9 @@ const { createSocket } = require('dgram')
 require('dotenv').config()
 
 
-try {
-    mongoose.connect(
-        "mongodb://127.0.0.1:27017/chat", {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    },
-    )
-} catch (e) {
-    console.log('failed')
-}
-
 // try {
 //     mongoose.connect(
-//         process.env.MONGO_URL, {
+//         "mongodb://127.0.0.1:27017/chat", {
 //         useNewUrlParser: true,
 //         useUnifiedTopology: true
 //     },
@@ -41,6 +30,17 @@ try {
 // } catch (e) {
 //     console.log('failed')
 // }
+
+try {
+    mongoose.connect(
+        process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    },
+    )
+} catch (e) {
+    console.log('failed')
+}
 
 app.use(express.json())
 app.use(cookieParser())
